1adacb00f5b859e9fc11cdedf60d0a5a3a4b85c6
drewdinger <drewdinger@users.noreply.github.com>
2014-09-17 13:47:12 -0600

    Update hello.java

--- 3308RecLab4/src/helloworld/hello.java
+++ 3308RecLab4/src/helloworld/hello.java
@@ -2,6 +2,7 @@ package helloworld;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); //Display the string
-		System.out.println("How are you?);
+		System.out.println("How are you?");
+		System.out.println("hey");
 	}
 }
